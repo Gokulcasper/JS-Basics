6fd6116c56c54a9184963f7fd0b6ca6f3cafce8b
Gokulcasper <ronitcasper215@gmail.com>
2024-06-18 21:45:13 +0530

    Learned Primitive Datatypes

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-// let name="Gokul";
-// let msg="Hi,My Name is " + name + " I'm a MERN Stack Developer"
-
+// var name="Gokul";
+// var msg="Hi,My Name is " + name + " I'm a MERN Stack Developer"
 // alert(msg);
 // console.log(msg);
-// let name="Anbu";
-// console.log(name); 
-let num1=5;
-let num2=45;
-let result=num1+num2;
-console.log(result);
+
+
+// let num1=5;
+// let num2=45;
+// let result=num1+num2;
+// console.log(result);
+
+// const name="gokulraj";
+// const dob="25-06-1998"
+// let msg="Hello My Name is " + name + " & I born on "+ dob +" I'm learning JS";
+// console.log(msg);
+
+let firstname="Gokul"; //String
+let age=24; //Integer
+let isYoung=true; //Boolean
+let lastName; //Undefined
+let trophy=null; //Null
