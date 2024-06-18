952b0a15307adda68bed1e75f0fb5707797385d3
Gokulcasper <ronitcasper215@gmail.com>
2024-06-18 21:26:19 +0530

    Learned JS Variables

---
+++ index.js
@@ -0,0 +1,11 @@
+// let name="Gokul";
+// let msg="Hi,My Name is " + name + " I'm a MERN Stack Developer"
+
+// alert(msg);
+// console.log(msg);
+// let name="Anbu";
+// console.log(name); 
+let num1=5;
+let num2=45;
+let result=num1+num2;
+console.log(result);
